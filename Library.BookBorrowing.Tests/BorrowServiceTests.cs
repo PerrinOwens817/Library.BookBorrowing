fb51195e33bdb0b4e682c4af888d4ba6c1cf0b8c
PerrinOwens <po00032@my.westga.edu>
2025-04-21 19:58:14 -0400

    Fixed the duplicate unit test.

---
+++ Library.BookBorrowing.Tests/BorrowServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using Library.BookBorrowing.Services;
+using Xunit;
+
+namespace Library.BookBorrowing.Tests
+{
+	public class BorrowServiceTests
+	{
+		[Fact]
+		public void CheckAvailability_ShouldReturnTrue_WhenBookIsAvailable()
+		{
+			var service = new BorrowService();
+			var result = service.CheckAvailability(1);
+			Assert.True(result);
+		}
+	}
+}
